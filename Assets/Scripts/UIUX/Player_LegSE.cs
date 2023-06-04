0f294a3848d6fc95d69066d1180ff2cb6123be66
aki <anbeiqingzi@gmail.com>
2023-06-05 01:11:00 +0900

    足音をほかクライアントでも共有できるようにしました。

--- Assets/Scripts/UIUX/Player_LegSE.cs
+++ Assets/Scripts/UIUX/Player_LegSE.cs
@@ -9,16 +9,10 @@ public class Player_LegSE : MonoBehaviourPunCallbacks
 {
     private AudioSource audioSource; // AudioSource取得.
     void Start() {
-        if(!photonView.IsMine) {
-            return;
-        }
         audioSource = GetComponent<AudioSource>(); // AudioSourceComponent取得.
     }
 
     private void OnTriggerEnter(Collider collider) {
-        if(!photonView.IsMine) {
-            return;
-        }
         if(collider.gameObject.tag == "Floor") {
             audioSource.Play(); // 音を鳴らす.
         }
