3654b15a49f3632f8278520a66d8a28f2b4890a2
aki <anbeiqingzi@gmail.com>
2023-07-12 21:45:05 +0900

    固有性能を基底クラスに統合したためそれらの処理や宣言を削除.御札ヒット時の処理も変更.

--- Assets/Scripts/Player/Derivation/Escape/EscapeWenrui.cs
+++ Assets/Scripts/Player/Derivation/Escape/EscapeWenrui.cs
@@ -5,12 +5,6 @@ using Photon.Pun;
 
 public class EscapeWenrui : PlayerEscape
 {
-    private List<GameObject> billList = new List<GameObject>(); // 御札のリスト.
-    public int billAmount = 6; // 同時に展開する御札の枚数.
-    private float radius = 5.0f; // 展開する距離.
-    private float abilityTime = 20.0f; // 固有能力の発動時間.
-    private float rotationSpeed = 100.0f; // 展開した御札が回転する速度.
-    private float coolTime = 10.0f; // クールタイム.
     void Start() {
         if(photonView.IsMine) {
             // 自分が鬼なら.
@@ -77,51 +71,5 @@ public class EscapeWenrui : PlayerEscape
     [PunRPC]
     private void WenruiES() {
         Destroy(this); // 削除.
-    }
-
-    //------ 以下、固有性能 ------//
-    private IEnumerator BillCircle() {
-        var deltaTime = 0.0f;
-        billList = new List<GameObject>();
-
-        for(int i = 0; i < billAmount; i++) {
-            float angle = i * (360f / billAmount);  // 角度を計算
-
-            // 角度に基づいて配置する位置を計算
-            Vector3 position = transform.position + Quaternion.Euler(0f, angle, 0f) * Vector3.forward * 0.1f;
-
-            // オブジェクトを配置
-            var tmpBill = PhotonNetwork.Instantiate("Bill", position, Quaternion.identity);
-            billList.Add(tmpBill);
-        }
-
-        var tmpRadius = 0.0f; // 膨張する展開距離.
-
-        while(deltaTime < abilityTime) {
-            // 指定の範囲まで展開されるまで広がる.
-            if(tmpRadius < radius) {
-                tmpRadius += 0.1f;
-            }
-            for(int i = 0; i < billAmount; i++) {
-                var relative = billList[i].transform.position - transform.position; // 方向ベクトル.
-                relative = relative.normalized; // 正規化.
-                billList[i].transform.position = transform.position + (relative * tmpRadius); // 方向ベクトルに距離を乗算し展開.
-                billList[i].transform.RotateAround(transform.position, Vector3.up, (i + 1) * rotationSpeed * Time.deltaTime); // 回転.
-            }
-            deltaTime += Time.deltaTime;
-            yield return null;
-        }
-
-        billDestroy();
-
-        // 発動終了.
-        isUseAvility = false;
-        StartCoroutine(AvillityCoolTime(coolTime));
-    }
-
-    private void billDestroy() {
-        foreach(var bill in billList) {
-            PhotonNetwork.Destroy(bill); // 展開した御札を破棄.
-        }
     }
 }
